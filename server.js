bf08d01c0716a7a359fae425049e3ec8e667c663
ShanJiang1911 <johnson33399@gmail.com>
2021-06-17 21:27:15 -0700

    lab08 functional

--- server.js
+++ server.js
@@ -1,97 +1,69 @@
 const express = require('express');
-
 const app = express();
-
 const axios = require('axios');
-
 require('dotenv').config();
-
 const cors = require('cors');
+const { response } = require('express')
 app.use(cors());
-
 const PORT = process.env.PORT || 3002;
-
 const key = process.env.WEATHER_API_KEY
-
 const movieKey = process.env.MOVIE_API_KEY
 
 //Above are reuquired basics
 //----------------------------------------
 
-const weatherData = await axios.get(`https://api.weatherbit.io/v2.0/current?key=${key}&lat=${lat}&lon=${lon}&format=json`);
-
-const movieData = await axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${movieKey}`)
-
 app.get('/', (req, res) => {
     res.send('hello from Hooah server!');
 });
 
-app.get('/movies', (req, res) => {
-
-    let cityMovieData = movieData.find(city => city.city_name === searchQuery)
-    
-    let movieDataPrettified = cityMovieData.map(obj => new MovieShow(``))
-    res.send(movieDataPrettified);
-
-});
-
-class MovieShow {
-    constructor(movieName, movieDescription) {
-        this.movieName = movieName;
-        this.movieDescription = movieDescription;
-    }
-}
-
-
-
-app.get('/weather', (req, res) => {
+app.get('/weather', async(req, res) => {
     //grab the data from the query
     let lat = req.query.lat;
     let lon = req.query.lon;
-    let searchQuery = req.query.searchQuery;
-
-    let cityWeatherData = weatherData.find(city => city.city_name === searchQuery);
-    
-    console.log(cityWeatherData);
-
-    if (cityWeatherData === undefined) {
-        res.status(500).send({error: 'Unsupported city'});
-    } else {
-        let cityDataPrettified = cityWeatherData.data.map(obj => new Forecast(`${obj.ob_time}, Temperature ${obj.app_temp}, Wind Speed ${obj.wind_spd} with ${obj.weather.description.toLowerCase()}`) )
+    let weatherData = await axios.get(`http://api.weatherbit.io/v2.0/forecast/daily?key=${key}&lat=${lat}&lon=${lon}`);
+    console.log(weatherData.data);
+    try {
+        let cityDataPrettified = weatherData.data.data.map(obj => new Forecast(obj));
         res.send(cityDataPrettified);
-    }
+        }
+    catch {
+        res.status(500).send({error: 'Unsupported city'});
+    } 
 });
 
 class Forecast {
-    constructor(date, description) {
-        this.date = date;
-        this.description = description;
+    constructor(forecastObj) {
+        this.date = forecastObj.datetime;;
+        this.description = `Low of ${forecastObj.low_temp}, high of ${forecastObj.high_temp} with ${forecastObj.weather.description.toLowerCase()}, Wind Speed ${forecastObj.wind_spd}`;
+    };
+};
+
+app.get('/movies', async(req, res) => {
+
+    let city = req.query.city;
+    try {
+        let movieData = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${movieKey}&query=${city}&page=1&include_adult=false`)
+        res.send(movieData.data.results.map(movie => new MovieShow(movie)));
+    } catch {
+        res.status(500).send('Sorry, nothing here');
+    }
+});
+
+class MovieShow {
+    constructor(movieObject) {
+        this.title = movieObject.title;
+        this.overview=movieObject.overview;
+        this.average_votes=movieObject.vote_average;
+        this.total_votes=movieObject.vote_count;
+        this.image_url=`https://image.tmdb.org/t/p/w500${movieObject.poster_path}`;
+        this.popularity=movieObject.popularity;
+        this.release_date=movieObject.release_date
     }
 }
 
-
-
-
-
+app.get('/*', (request, reponse) => {
+    response.status(404).send('Looks like nothing here')
+});
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
-
-
-
-
-// app.get('/potato', (request, response) => {
-//     response.send('potatoes are delicious');
-// });
-
-
-
-// weatherArray.find(item => {
-//     if(item.city_name === searchQuery){
-//         //probably return whole weather set ie 'item'
-//     return true;
-//     } else {
-//         //probably return error message
-//         return false
-//     }
-// })
 
